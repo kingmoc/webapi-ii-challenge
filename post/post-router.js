6a9a9f8e92badddacd248614eaa195963d55a4ec
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-06 17:18:41 -0500

    GET /api/posts complete

--- post/post-router.js
+++ post/post-router.js
@@ -2,7 +2,13 @@ const router = require('express').Router()
 const Posts = require('../data/db')
 
 router.get('/', (req, res) => {
-    res.send('Router Works')
+    Posts.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(500).json( { error: "The posts information could not be retrieved." })
+        })
 });
 
 
